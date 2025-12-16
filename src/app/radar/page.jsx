87a9750225bfb816fa47f12385697631959a22d4
DAMIANHE <66206811+DAMIANHE@users.noreply.github.com>
2025-12-16 10:31:09 -0300

    Update page.jsx

--- src/app/radar/page.jsx
+++ src/app/radar/page.jsx
@@ -1,1 +1,11 @@
+import VefsRadarContainer from "@/components/VefsRadarContainer";
+
+export default function RadarPage() {
+  return (
+    <main>
+      <h1>Radar VEFS</h1>
+      <VefsRadarContainer />
+    </main>
+  );
+}
 
